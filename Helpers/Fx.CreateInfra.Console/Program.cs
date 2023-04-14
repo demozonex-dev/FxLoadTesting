92a832029b97d9f120ef9ee909ebfdca00ac7c90
Eric Vernié <eric.vernie@hotmail.fr>
2023-04-14 17:48:50 +0200

    adding create resources

--- Helpers/Fx.CreateInfra.Console/Program.cs
+++ Helpers/Fx.CreateInfra.Console/Program.cs
@@ -5,4 +5,41 @@ using Fx.ArmManager;
 
 ResourceClient resourceClient = new ResourceClient();
 resourceClient.Login(await Fx.Helpers.Identity.AuthenticateAsync(Fx.Helpers.AuthenticationType.DeviceCode));
+await resourceClient.SetDefaultSubscriptionAsync();
 
+var config = Fx.Helpers.Configuration.Create();
+var parametersSection = config.GetSection("parameters");
+string? resourceGroupName = parametersSection["resourcegroup:value"];
+if (resourceGroupName == null) { throw new NullReferenceException(nameof(resourceGroupName)); }
+
+
+string? location = parametersSection["location:value"];
+if (location==null) { throw new NullReferenceException(nameof(location)); }
+Console.WriteLine("Creating Resource Group");
+await resourceClient
+            .CreateOrUpdateResourceGroupAsync(resourceGroupName, 
+                                              location);
+
+string? topicName = parametersSection["eventgridtopic:value"];
+if (topicName == null) { throw new NullReferenceException(nameof(topicName)); }
+
+string? subscriptionName = parametersSection["eventgridsubscription:value"];
+if (subscriptionName == null) { throw  new NullReferenceException(nameof(subscriptionName)); }
+
+string? hybridConnection = parametersSection["hybridconnection:value"];
+if (hybridConnection==null) { throw new NullReferenceException(nameof(hybridConnection)); }
+string? relayNamespace = parametersSection["relaynamespace:value"];
+if (relayNamespace==null) { throw new NullReferenceException(nameof(relayNamespace)); }
+
+Console.WriteLine("Creating Relay");
+await resourceClient
+            .CreateOrUpdateRelayNamespaceAsync(relayNamespace,
+                                               hybridConnection,
+                                               location);
+Console.WriteLine("Creating event grid");
+await resourceClient
+                   .CreateOrUpdateEventGridTopicAsync(topicName,
+                                                      subscriptionName,
+                                                      location);
+
+Console.WriteLine("Success !!!!");
