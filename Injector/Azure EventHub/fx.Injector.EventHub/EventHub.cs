900d0a17c608146b174e02aab6cd565b00458498
Eric Vernié <eric.vernie@hotmail.fr>
2023-05-17 15:28:18 +0200

    adding event hub (not finish)

---
+++ Injector/Azure EventHub/fx.Injector.EventHub/EventHub.cs
@@ -0,0 +1,14 @@
+﻿using Fx.Injector;
+
+namespace fx.Injector
+{
+    public class EventHub : IInjector
+    {
+        public string InjectorType => throw new NotImplementedException();
+
+        public Task SendAsync(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Receiver/Azure EventHub/fx.Receiver.EventHub/EventHub.cs
@@ -0,0 +1,19 @@
+﻿using Fx.Receiver;
+
+namespace fx.Receiver
+{
+    public class EventHub : ReceiverBase, IReceiver
+    {
+        public string ReceiverType => throw new NotImplementedException();
+
+        public Task StartAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
